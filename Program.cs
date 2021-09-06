2580ee6266be5e09f21db45b7ae55bf39dd6fa57
Sanier <Sanier20@mail.ru>
2021-09-06 19:31:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Obj_4._3._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { {1,2,3}, {4,5,6}, {7,8,9}, {10,11,12} };
+
+            for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+            {
+                for (int j = 0; j < array.GetUpperBound(0) + 1; j++)
+                    Console.Write(array[j, i] + " ");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
